a634dc801131fbdca6eb85030a30407eeb792dc0
INZERO12 <jikoung1@naver.com>
2022-01-19 17:34:51 +0900

    1월 19일 디버깅까지 커밋

---
+++ day4/input_exam.py
@@ -0,0 +1,16 @@
+# 입력 테스트
+# a = input()
+# print(f'입력값은 {a}')
+
+# # 입출력 같이
+# number = int(input('수를 입력하세요 :'))
+# print(number * 3)
+
+# Escape 문자
+print('Life is short. You need Python.')
+print('Life is short.\rYou need Python.')     # \r 앞으로 땡기기
+print('Life is short.\nYou need Python.')     # \n 밑으로 내리기
+print('Life is short. You \tneed Python.')    # \t tab
+print('Life is short. You need \'Python\'.')  # \'단어\' 
+print('Life is short. You\b need Python.')    # \b 한 줄 앞으로
+print('Life is \"short\". You need Python.')  #\"단어\"
